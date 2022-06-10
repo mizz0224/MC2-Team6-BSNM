bb3c7b51da54a94657305d9495998ca8c050b140
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-06-10 16:11:41 +0900

    Req 프로젝트 생성

---
+++ Req/Req/ReqApp.swift
@@ -0,0 +1,20 @@
+//
+//  ReqApp.swift
+//  Req
+//
+//  Created by 이재웅 on 2022/06/10.
+//
+
+import SwiftUI
+
+@main
+struct ReqApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
