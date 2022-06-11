dfdb1ca0692a4e64837fb7b5959919f9288977a9
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-06-11 23:58:26 +0900

    3-feat-feedback-view: AddFeedbackView 생성 및 FeedbackImage, FeedbackBottomView 추가

---
+++ Req/Req/AddFeedbackView.swift
@@ -0,0 +1,27 @@
+//
+//  AddFeedbackView.swift
+//  Req
+//
+//  Created by 이재웅 on 2022/06/11.
+//
+
+import SwiftUI
+
+struct AddFeedbackView: View {
+    var body: some View {
+        VStack {
+            FeedbackImage()
+            
+            Spacer()
+                .frame(height: 34.0)
+            
+            FeedbackBottomView()
+        }
+    }
+}
+
+struct AddFeedbackView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddFeedbackView()
+    }
+}
