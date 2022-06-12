693ece0aed8963159b2c5906d1a34f256d1fc29b
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-06-12 15:48:43 +0900

    3-feat-feedback-view: FeedbackTitleTextField 생성

---
+++ Req/Req/AddFeedback/FeedbackTitleTextField.swift
@@ -0,0 +1,21 @@
+//
+//  FeedbackTitleTextField.swift
+//  Req
+//
+//  Created by 이재웅 on 2022/06/12.
+//
+
+import SwiftUI
+
+struct FeedbackTitleTextField: View {
+    @Binding var title: String
+    let titlePlaceHolder: String = "무엇에 대한 피드백인가요?"
+    
+    var body: some View {
+        TextField("무엇에 대한 피드백인가요?", text: $title)
+            .textFieldStyle(.plain)
+            .disableAutocorrection(true)
+            .font(.system(size: 24.0, weight: .bold))
+            
+    }
+}
