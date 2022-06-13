8af3404686684cba82f3d390543ae468038e3ef3
Inho Choi <eknow25@gmail.com>
2022-06-13 16:17:52 +0900

    [Feat] Camera Representable

--- Req/Req/Camera/CameraRepresentable.swift
+++ Req/Req/Camera/CameraRepresentable.swift
@@ -50,3 +50,88 @@ struct CameraRepresentable: UIViewControllerRepresentable {
     }
 }
 
+
+class CameraController: UIViewController {
+    
+    var image: UIImage?
+    
+    var captureSession = AVCaptureSession()
+    var backCamera: AVCaptureDevice?
+    var frontCamera: AVCaptureDevice?
+    var currentCamera: AVCaptureDevice?
+    var photoOutput: AVCapturePhotoOutput?
+    var cameraPreviewLayer: AVCaptureVideoPreviewLayer?
+    
+    //DELEGATE
+    var delegate: AVCapturePhotoCaptureDelegate?
+    
+    func didTapRecord() {
+        
+        let settings = AVCapturePhotoSettings()
+        photoOutput?.capturePhoto(with: settings, delegate: delegate!)
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+    func setup() {
+        setupCaptureSession()
+        setupDevice()
+        setupInputOutput()
+        setupPreviewLayer()
+        startRunningCaptureSession()
+    }
+    func setupCaptureSession() {
+        captureSession.sessionPreset = AVCaptureSession.Preset.photo
+    }
+    
+    func setupDevice() {
+        let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [AVCaptureDevice.DeviceType.builtInWideAngleCamera],
+                                                                      mediaType: AVMediaType.video,
+                                                                      position: AVCaptureDevice.Position.unspecified)
+        for device in deviceDiscoverySession.devices {
+            
+            switch device.position {
+            case AVCaptureDevice.Position.front:
+                self.frontCamera = device
+            case AVCaptureDevice.Position.back:
+                self.backCamera = device
+            default:
+                break
+            }
+        }
+        
+        self.currentCamera = self.backCamera
+    }
+    
+    
+    func setupInputOutput() {
+        do {
+            
+            let captureDeviceInput = try AVCaptureDeviceInput(device: currentCamera!)
+            captureSession.addInput(captureDeviceInput)
+            photoOutput = AVCapturePhotoOutput()
+            photoOutput?.setPreparedPhotoSettingsArray([AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecType.jpeg])], completionHandler: nil)
+            captureSession.addOutput(photoOutput!)
+            
+        } catch {
+            print(error)
+        }
+        
+    }
+    func setupPreviewLayer()
+    {
+        self.cameraPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        self.cameraPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        self.cameraPreviewLayer?.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
+        self.cameraPreviewLayer?.frame = self.view.frame
+        self.view.layer.insertSublayer(cameraPreviewLayer!, at: 0)
+        
+    }
+    func startRunningCaptureSession(){
+        captureSession.startRunning()
+    }
+}
+
