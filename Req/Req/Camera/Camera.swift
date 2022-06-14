441116274c62bfd60fd9ba0e81bb7f17c1cad6e8
Inho Choi <eknow25@gmail.com>
2022-06-14 22:30:03 +0900

    [Feat] UIImage Rotate

--- Req/Req/Camera/Camera.swift
+++ Req/Req/Camera/Camera.swift
@@ -101,7 +101,7 @@ extension Camera: AVCapturePhotoCaptureDelegate {
 }
 
 func cropImage(_ inputImage: UIImage) -> UIImage? {
-    let image = inputImage
+    let image = inputImage.rotate(radians: 0)!
     let cropRect = CGRect(
         x: CGFloat(0.0),
         y: CGFloat(image.size.height * (100 / 844) + 13),
@@ -118,4 +118,28 @@ func cropImage(_ inputImage: UIImage) -> UIImage? {
     // Return image to UIImage
     let croppedImage: UIImage = UIImage(cgImage: cutImageRef)
     return croppedImage
+}
+
+extension UIImage {
+    func rotate(radians: Float) -> UIImage? {
+        var newSize = CGRect(origin: CGPoint.zero, size: self.size).applying(CGAffineTransform(rotationAngle: CGFloat(radians))).size
+        // Trim off the extremely small float value to prevent core graphics from rounding it up
+        newSize.width = floor(newSize.width)
+        newSize.height = floor(newSize.height)
+
+        UIGraphicsBeginImageContextWithOptions(newSize, false, self.scale)
+        let context = UIGraphicsGetCurrentContext()!
+
+        // Move origin to middle
+        context.translateBy(x: newSize.width / 2, y: newSize.height / 2)
+        // Rotate around middle
+        context.rotate(by: CGFloat(radians))
+        // Draw the image at its center
+        self.draw(in: CGRect(x: -self.size.width / 2, y: -self.size.height / 2, width: self.size.width, height: self.size.height))
+
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        return newImage
+    }
 }
