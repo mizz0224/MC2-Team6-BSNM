8f07f6fddf65c8859ecb3af170f2b1ee1d26a698
Inho Choi <eknow25@gmail.com>
2022-06-15 00:48:01 +0900

    [Feat] FeedbackCameraView Navigation 연결

--- Req/Req/FeedbackCameraView.swift
+++ Req/Req/FeedbackCameraView.swift
@@ -11,27 +11,69 @@ struct FeedbackCameraView: View {
     @ObservedObject var viewModel = CameraViewModel()
 
     var body: some View {
-        VStack(spacing: 0) {
+        NavigationView {
+            VStack(spacing: 0) {
 
-            Rectangle()
-                .frame(height: 100)
-                .foregroundColor(.black)
-
-            viewModel.cameraPreview
-                .onAppear {
-                viewModel.configure()
-            }
-
-            ZStack {
                 Rectangle()
+                    .frame(height: 100)
                     .foregroundColor(.black)
 
-                Button(action: { viewModel.capturePhoto() }) {
-                    Image(systemName: "camera.circle")
+                if viewModel.recentImage == nil {
+                    viewModel.cameraPreview
+                        .onAppear {
+                        viewModel.configure()
+                    }
                 }
-            }.frame(height: 224)
+                else {
+                    Image(uiImage: viewModel.recentImage!)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(height: UIScreen.main.bounds.height * (520 / 844))
+                }
+
+                ZStack {
+                    Rectangle()
+                        .foregroundColor(.black)
+
+                    if viewModel.recentImage == nil {
+                        VStack {
+                            Text("얼굴을 제외하고 사진을 찍어주세요.")
+                                .font(.custom("Apple SD Gothic Neo Regular", size: 15))
+                                .foregroundColor(.white)
+                                .padding(EdgeInsets(top: 20, leading: 0, bottom: 0, trailing: 0))
+
+                            Spacer(minLength: 52)
+
+                            Button(action: { viewModel.capturePhoto() }) {
+                                Image("cameraButton")
+                            }
+                            Spacer(minLength: 52)
+                        }
+                    } else {
+                        VStack {
+                            Spacer()
+
+                            HStack {
+                                Button(action: { viewModel.recentImage = nil }) {
+                                    Text("다시 찍기")
+                                        .font(.custom("Apple SD Gothic Neo Bold", size: 18))
+                                        .foregroundColor(.white)
+                                        .padding(EdgeInsets(top: 0, leading: 28, bottom: 60, trailing: 0))
+                                }
+                                Spacer()
+                                NavigationLink(destination: { /*TODO: 다음 View*/ }) {
+                                    Text("피드백 하기")
+                                        .font(.custom("Apple SD Gothic Neo Bold", size: 18))
+                                        .foregroundColor(.white)
+                                        .padding(EdgeInsets(top: 0, leading: 0, bottom: 60, trailing: 28))
+                                }
+                            }
+                        }
+                    }
+                }.frame(height: 224)
+            }
+                .ignoresSafeArea()
         }
-            .ignoresSafeArea()
     }
 }
 
