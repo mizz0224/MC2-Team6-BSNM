0d42bfb32a160d7f6687cf5c43bb201c7f0ea63e
ocner1435 <67146824+ocner1435@users.noreply.github.com>
2022-06-16 00:50:01 +0900

    1-feat-home&detailView:  슬로건 구현

--- Req/Req/New Group/Views/PostGridView.swift
+++ Req/Req/New Group/Views/PostGridView.swift
@@ -8,31 +8,29 @@
 import SwiftUI
 
 struct PostGridView: View {
-    // MARK:- PROPERTIES
+
     
     let gridLayout:[GridItem] =  Array(repeating: .init(.fixed(160), spacing: 10), count: 2)
-    
     let data: [PostModel]
     
-    // MARK:- BODY
     
     var body: some View {
+        //그리드
         LazyVGrid(columns: gridLayout, alignment: .center, spacing: 0){
             
             ForEach(data) { item in
                 ZStack(alignment: .center){
                     Image(item.image)
                         .resizable()
-                    //  .scaledToFill()
                         .frame(width: 160, height: 213.3)
                         .clipped()
-                       
+                    
                     Text("Feedback from \n \(item.name)")
                         .padding(.init(top: 170, leading: 5, bottom: 10, trailing: 10))
                         .foregroundColor(.white)
                 }
-            }//: LOOP
-        }//: GRID
+            }
+        }
         
     }
 }
