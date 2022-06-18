493b36194da839f935a690656a3bf61e99350ad1
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-06-18 20:44:06 +0900

    [Fix] 피드백 저장버튼 후 확인용 Alert 제거

--- Req/Req/AddFeedback/FeedbackNavigationItems/AddFeedbackNavigationItem/AddFeedbackNavigationSaveButton.swift
+++ Req/Req/AddFeedback/FeedbackNavigationItems/AddFeedbackNavigationItem/AddFeedbackNavigationSaveButton.swift
@@ -31,21 +31,13 @@ struct AddFeedbackNavigationSaveButton: View {
         .alert("피드백을 저장하시겠습니까?", isPresented: $saveAlertShow) {
             Button("취소", role: .destructive) { saveAlertShow = false }
             Button("저장하기", role: .cancel) {
-                saveAlertShow = false
-                dataAlertShow = true
-            }
-        } // 저장 Alert
-        .alert(
-            "현재 시간 : \(getDate())\n작성자 : \(reviewerName)\n이미지 : \(image)\n핀 개수 : \(pins.count)",
-            isPresented: $dataAlertShow
-        ) {
-            Button("확인", role: .cancel) {
+                print("현재 시간 : \(getDate())\n작성자 : \(reviewerName)\n이미지 : \(image)\n핀 개수 : \(pins.count)")
                 saveData()
                 
-                dataAlertShow = false
+                saveAlertShow = false
                 showCameraView = false
             }
-        } // 확인 Alert
+        } // 저장 Alert
     }
 }
 
