309a7cc26517a579831bc90000c2287097e53279
Inho Choi <eknow25@gmail.com>
2022-06-18 23:36:39 +0900

    [Fix] SearchBar UI 수정

--- Req/Req/View/HomeView/SearchBar.swift
+++ Req/Req/View/HomeView/SearchBar.swift
@@ -12,55 +12,45 @@ struct SearchBar: View {
     @Binding var searchText: String
     @Binding var isSearching: Bool
     @Binding var showResults: Bool
-    
+
     var body: some View {
-        
+
         VStack (spacing: 0) {
             Spacer()
             HStack(spacing: 0) {
-                Spacer().frame(width:8)
+                Spacer().frame(width: 8)
                 
-                HStack(spacing: 0 ) {
+                
+                
+                HStack(spacing: 0) {
+                    Image(systemName: "magnifyingglass")
+                        .padding(.trailing, 4)
+                    
                     TextField("패션 아이템의 이름을 입력하세요", text: $searchText)
-                        .padding(.leading, 24)
                 }
-                .frame(height:20)
-                .padding(.horizontal)
-                .background(.gray)
-                .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
-                .onTapGesture(perform: {
+                    .frame(height: 20)
+                    .padding(.horizontal)
+                    .foregroundColor(.black)
+                    .background(Color(red: 238 / 255, green: 238 / 255, blue: 238 / 255, opacity: 1))
+                    .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
+                    .onTapGesture(perform: {
                     isSearching = true
                     showResults = true
                 })
-                .overlay (
-                    HStack {
-                        Image(systemName: "magnifyingglass")
-                        Spacer()
-                        
-                        Button(action: {searchText = ""}, label: {
-                            Image(systemName: "xmark.circle.fill")
-                                .padding(.vertical)
-                                .foregroundColor(.gray)
-                                .opacity(isSearching ? 1 : 0)
-                        })
-                    }
-                    .padding(.horizontal, 32)
-                    .foregroundColor(.gray)
-                )
-                
+
                 Spacer()
-                
-                Button(action: {self.getisSearch = false}) {//홈으로 이동
+
+                Button(action: { self.getisSearch = false }) { //홈으로 이동
                     Image(systemName: "xmark.circle.fill")
                         .foregroundColor(.gray)
                 }//뒤로가기 버튼
-                
-                Spacer().frame(width:32)
+
+                Spacer().frame(width: 32)
             }
-            Spacer().frame(height:18)
+            Spacer().frame(height: 18)
         }
     }
-    
+
 }
 
 
