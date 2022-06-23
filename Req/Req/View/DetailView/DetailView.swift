fd28b545c367993053b689caadccdff028f9f176
Inho Choi <eknow25@gmail.com>
2022-06-23 15:28:51 +0900

    [Fix] DetailView Ratio

--- Req/Req/View/DetailView/DetailView.swift
+++ Req/Req/View/DetailView/DetailView.swift
@@ -23,29 +23,28 @@
 //}
 import SwiftUI
 struct DetailView: View {
-    
-    
-    let MenuFont : Font = Font.system(size:20) //메뉴 폰트 사이즈
-    let totalFrameHeight : CGFloat = 796
-    let getFeedback : Feedback//받아온 피드백 데이터
-    let detailViewBgColor : Color = Color(red: 251/255, green: 251/255, blue: 255/255)//상세페이지 배경 색깔 : reqWhite : fdfdff
-    @Binding var isDark : Bool//다크모드를 제어할 변수
+
+
+    let MenuFont: Font = Font.system(size: 20) //메뉴 폰트 사이즈
+    let totalFrameHeight: CGFloat = 796
+    let getFeedback: Feedback//받아온 피드백 데이터
+    let detailViewBgColor: Color = Color(red: 251 / 255, green: 251 / 255, blue: 255 / 255)//상세페이지 배경 색깔 : reqWhite : fdfdff
+    @Binding var isDark: Bool//다크모드를 제어할 변수
     var body: some View {
-        VStack(spacing:0) {
-            Spacer().frame(height:100)//상단 공백
+        VStack(spacing: 0) {
+            Spacer()
+                .frameRatio(height: 100)//상단 공백
+            
             TopMenuView(idToDelete: getFeedback.id, feedback: getFeedback)//뒤로가기, 삭제 버튼 있는 뷰
             ImageView(getFeedback: getFeedback)//이미지 뷰, 하단 설명뷰는 이미지 뷰에서 구현
-        }.frame(height:totalFrameHeight)
-        .onAppear(){
-            isDark = false//상세 페이지가 등장 할때 다크모드 해제
-        }.ignoresSafeArea().background(detailViewBgColor)//VStack
+        }
+            .frameRatio(height: totalFrameHeight)
+            .onAppear() {
+                isDark = false//상세 페이지가 등장 할때 다크모드 해제
+            }
+            .ignoresSafeArea()
+            .background(detailViewBgColor)//VStack
     }//body
-//    struct DetailView_Previews: PreviewProvider {
-//        static var previews: some View {
-//            DetailView(getFeedback: testData)
-//        }
-//    }
-    
 }
 
 extension UINavigationController: UIGestureRecognizerDelegate {
--- Req/Req/View/DetailView/TopMenuView.swift
+++ Req/Req/View/DetailView/TopMenuView.swift
@@ -7,60 +7,58 @@
 
 import SwiftUI
 
-struct TopMenuView : View {
+struct TopMenuView: View {
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     @EnvironmentObject var userData: UserDataManager
-    
-    @State private var showingAlert : Bool = false //alert 띄워주기 위한 변수
+
+    @State private var showingAlert: Bool = false //alert 띄워주기 위한 변수
     @State var navigateNext = false
-    let MenuFont : Font = Font.system(size:20).bold() //메뉴(뒤로가기,삭제) 버튼 폰트
-    let idToDelete : UUID //삭제될 피드백의 아이디(UUID)
-    let TopMenuIconColor : Color = Color(red: 13/255, green: 13/255, blue: 14/255)//상단메뉴 아이콘 색깔 : reqBlack : 0d0de
-    let TopMenuBgColor : Color = Color(red: 251/255, green: 251/255, blue: 255/255)//상단메뉴 배경 색깔 : reqWhite : fdfdff
+    let MenuFont: Font = Font.system(size: 20).bold() //메뉴(뒤로가기,삭제) 버튼 폰트
+    let idToDelete: UUID //삭제될 피드백의 아이디(UUID)
+    let TopMenuIconColor: Color = Color(red: 13 / 255, green: 13 / 255, blue: 14 / 255)//상단메뉴 아이콘 색깔 : reqBlack : 0d0de
+    let TopMenuBgColor: Color = Color(red: 251 / 255, green: 251 / 255, blue: 255 / 255)//상단메뉴 배경 색깔 : reqWhite : fdfdff
     let feedback: Feedback
-    
-    var body : some View {
-        
-        VStack(alignment: .center, spacing: 0){
-            HStack(spacing:0) {
-                Button(action: { gotoHome() }) {//홈으로 이동
+
+    var body: some View {
+
+        VStack(alignment: .center, spacing: 0) {
+            HStack(spacing: 0) {
+                Button(action: { gotoHome() }) { //홈으로 이동
                     Image(systemName: "chevron.backward")
                         .font(MenuFont)
-                        .frame(height: 24)
+                        .frameRatio(height: 24)
                         .foregroundColor(TopMenuIconColor)
                         .padding(EdgeInsets(top: 10, leading: 16, bottom: 18, trailing: 0))
                 }//뒤로가기 버튼
                 Spacer()//좌우 구분을 위한 공백
                 Button(action: { self.showingAlert.toggle() }) {
-                    Image(systemName : "trash.fill")
+                    Image(systemName: "trash.fill")
                         .font(MenuFont)
-                        .frame(height: 24)
+                        .frameRatio(height: 24)
                         .foregroundColor(TopMenuIconColor)
                         .padding(EdgeInsets(top: 10, leading: 0, bottom: 18, trailing: 16))
-                        .alert(isPresented: $showingAlert) {
-                            Alert(
-                                title: Text("해당 기록을 정말 삭제하시겠습니까?"),
-                                primaryButton: .destructive(Text("삭제"), action: {
-                                    deleteFeedback(targetUUID: idToDelete)
-                                    
-                                }),
-                                secondaryButton: .cancel(Text("취소")))
-                            //예, 아니오 버튼
-                        }//alert
+                        .alert(isPresented: $showingAlert) { Alert(
+                            title: Text("해당 기록을 정말 삭제하시겠습니까?"),
+                            primaryButton: .destructive(Text("삭제"), action: { deleteFeedback(targetUUID: idToDelete) }),
+                            secondaryButton: .cancel(Text("취소"))
+                        )
+                    } //alert
                 }//삭제 버튼
             }//Hstack
-        }.frame(height: 52).background(TopMenuBgColor)// Vstack
+        } // Vstack
+        .frameRatio(height: 52)
+            .background(TopMenuBgColor)
     }//TopMenuView
-    
-    
-    func gotoHome()->Void {
+
+
+    func gotoHome() -> Void {
         self.presentationMode.wrappedValue.dismiss()
     }
-    func deleteFeedback(targetUUID : UUID)->Void {
+    func deleteFeedback(targetUUID: UUID) -> Void {
         userData.deleteFeedback(feedbackID: feedback.id)
         self.presentationMode.wrappedValue.dismiss()
     }
-    
+
 }
 struct DestinationVC: View {
     var body: some View {
