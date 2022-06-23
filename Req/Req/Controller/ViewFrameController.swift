4e452255e6c4101a60fbd70bb43e44b86230d4a8
Inho Choi <eknow25@gmail.com>
2022-06-23 14:45:57 +0900

    [Fix] HomeView Ratio

--- Req/Req/Controller/ViewFrameController.swift
+++ Req/Req/Controller/ViewFrameController.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import SwiftUI
+
+extension View {
+    @ViewBuilder
+    func frameRatio(width: CGFloat? = nil, height: CGFloat? = nil) -> some View {
+        if width != nil && height != nil {
+            self.frame(
+                width: UIScreen.main.bounds.width * width! / CGFloat(390),
+                height: UIScreen.main.bounds.height * height! / CGFloat(844)
+            )
+        } else if width == nil {
+            self.frame(height: UIScreen.main.bounds.height * height! / CGFloat(844))
+        } else if height == nil {
+            self.frame(width: UIScreen.main.bounds.width * width! / CGFloat(390))
+        } else {
+            self
+        }
+    }
+}
+
